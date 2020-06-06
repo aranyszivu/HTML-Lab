4f394ed98be627669e3fdd4566946f795e5c4d11
aranyszivu <34288622+aranyszivu@users.noreply.github.com>
2020-06-06 19:03:45 -0400

    Add files via upload

---
+++ Resources/Scripts/groceries-lab3.js
@@ -0,0 +1,141 @@
+﻿
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+	{
+		name: "Broccoli",
+		vegetarian: true,
+		glutenFree: true,
+		price: 1.99,
+		category: "vegetables"
+	},
+	{
+		name: "Bread",
+		vegetarian: true,
+		glutenFree: false,
+		price: 2.35,
+		category: "grains"
+	},
+	{
+		name: "Chicken Breast",
+		vegetarian: false,
+		glutenFree: true,
+		price: 9.99,
+		category: "meat"
+	},
+	{
+		name: "Microwave Dinner",
+		vegetarian: false,
+		glutenFree: true,
+		price: 12.49,
+		category: "frozen"
+	},
+	{
+		name: "Raspberries",
+		vegetarian: true,
+		glutenFree: true,
+		price: 3.49,
+		category: "fruit"
+	},
+	{
+		name: "Ribeye Steak",
+		vegetarian: false,
+		glutenFree: true,
+		price: 15.45,
+		category: "meat"
+	},
+	{
+		name: "Olive Oil",
+		vegetarian: true,
+		glutenFree: true,
+		price: 6.99,
+		category: "cooking"
+	},
+	{
+		name: "Cod Fillet",
+		vegetarian: false,
+		glutenFree: true,
+		price: 8.99,
+		category: "meat"
+	},
+	{
+		name: "Bagels",
+		vegetarian: true,
+		glutenFree: false,
+		price: 4.99,
+		category: "grains"
+	},
+	{
+		name: "Breaded Chicken Wings",
+		vegetarian: false,
+		glutenFree: false,
+		price: 11.99,
+		category: "frozen"
+	}
+];
+
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, restriction) {
+	let product_names = []; 
+	for (let i = 0; i < prods.length; i += 1) {
+		if (prods[i].vegetarian == false && restriction[0]) {
+			continue;
+		}
+		if (prods[i].glutenFree == false && restriction[1]) {
+			continue;
+		}
+		product_names.push(prods[i]);
+	} 
+	
+	return product_names;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i = 0; i < products.length; i += 1) {
+		if (chosenProducts.indexOf(products[i].name) > -1) {
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice;
+}
+
+function changeOrganic(pref) {
+	var flag = document.getElementById("prefOrganic");
+	if (document.getElementById("organicPrefYes").checked) {
+		flag.innerHTML = "<i>Organic Items will be included if available!</i>";
+	}
+	else {
+		flag.innerHTML = "";
+	}
+}
+
+function filterItems() {
+	var includes = [
+		document.getElementById("fltrFruit").checked,
+		document.getElementById("fltrVeg").checked,
+		document.getElementById("fltrGrain").checked,
+		document.getElementById("fltrMeat").checked,
+		document.getElementById("fltrFrozen").checked,
+		document.getElementById("fltrCooking").checked
+	]
+	var classes = [ "fruit", "vegetables", "grains", "meat", "frozen", "cooking" ];
+	//Hide/Show for each category
+	for (i = 0; i < includes.length; i++) {
+		var hideList = document.getElementsByClassName(classes[i]);
+		for (j = 0; j < hideList.length; j++) {
+			if (!includes[i]) {
+				hideList.item(j).style.display = "none";
+			}
+			else {
+				hideList.item(j).style.display = "";
+			}
+		}
+	}
+}
---
+++ Resources/Scripts/main-lab3.js
@@ -0,0 +1,164 @@
+﻿// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openInfo(tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Activates the desired tab button
+	document.getElementById(tabName).style.display = "block";
+	document.getElementById("btn" + tabName).className += " active";
+}
+
+
+
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices(slct2) {
+	var s2 = document.getElementById(slct2);
+
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+	//Initialize Empty Product Table
+	var productTable = document.createElement("table");
+	productTable.id = "productTable";
+
+	var tHeader = document.createElement("tr");
+	tHeader.id = "tableHeader"
+	var nameHdr = document.createElement("th");
+	nameHdr.innerHTML = "Product";
+	var priceHdr = document.createElement("th");
+	priceHdr.innerHTML = "Price";
+
+	tHeader.appendChild(document.createElement("th", {id: "imageCell"})); //Image Header
+	tHeader.appendChild(nameHdr);
+	tHeader.appendChild(priceHdr);
+	tHeader.appendChild(document.createElement("th")); //Selection Checkbox Header
+	productTable.appendChild(tHeader);
+
+	s2.appendChild(productTable);
+
+
+	// obtain a reduced list of products based on restrictions
+	var getVeg = document.getElementById("prefVeg").checked;
+	var getGlut = document.getElementById("prefGlut").checked;
+	var restrictions = [getVeg, getGlut]
+
+	var optionArray = restrictListProducts(products, restrictions);
+	//Order list by price
+	optionArray.sort( (a,b) => (a.price >= b.price) ? 1 : -1 );
+
+	//Generate table rows
+	/*	<tr id="oddRow/evenRow">
+	 *		<td><img src=".." /></td>
+	 *		<td>Name</td>
+	 *		<td>Price</td>
+	 *		<td><input type="checkbox" name="product" value=Name /></td>
+	 *	</tr>
+	 */
+	for (i = 0; i < optionArray.length; i++) {
+		var product = optionArray[i];
+		
+		var newRow = document.createElement("tr");
+		newRow.id = (i % 2 == 1) ? "oddRow" : "evenRow";
+		//Set id of row to category for filtering
+		newRow.className = product.category;
+
+		var imageThumb = document.createElement("img");
+		//If i were actually using individual images, I'd just search for 
+		//	"Resources/Images/"+ product.name + ".png"
+		//instead, but the method stays the same.
+		imageThumb.src = "Resources/Images/groceryLogo.png";
+		imageThumb.class = "groceryThumbImg";
+		var imgThumbWrapper = document.createElement("td");
+		imgThumbWrapper.appendChild(imageThumb);
+		imgThumbWrapper.id = "imageCell";
+		newRow.appendChild(imgThumbWrapper);
+
+		var productName = document.createElement("td");
+		productName.innerText = product.name;
+		newRow.appendChild(productName);
+
+		var productPrice = document.createElement("td");
+		productPrice.innerText = product.price;
+		newRow.appendChild(productPrice);
+
+		var checkBoxWrapper = document.createElement("td");
+		var checkBox = document.createElement("input");
+		checkBox.type = "checkbox";
+		checkBox.name = "product";
+		checkBox.value = product.name;
+		checkBoxWrapper.appendChild(checkBox);
+		newRow.appendChild(checkBoxWrapper);
+
+		productTable.appendChild(newRow);
+
+		//Update parameter in Produce div
+		var statusText = ""
+		if (getGlut && getVeg) {
+			statusText = "<i>Currently only showing Gluten-Free & Vegetarian options!</i>";
+		}
+		else if (!getGlut && getVeg) {
+			statusText = "<i>Currently only showing Vegetarian options!</i>";
+		}
+		else if (getGlut && !getVeg) {
+			statusText = "<i>Currently only showing Gluten-Free options!</i>";
+		}
+		document.getElementById("restrictions").innerHTML = statusText;
+	}
+}
+
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph) 
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems() {
+
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+
+	// build list of selected item
+	var productTable = document.createElement("table");
+	productTable.id = "productTable";
+	var tHeader = document.createElement("tr");
+	tHeader.id = "tableHeader"
+	var nameHdr = document.createElement("th");
+	nameHdr.innerHTML = "Product";
+	tHeader.appendChild(nameHdr);
+	productTable.appendChild(tHeader);
+	c.appendChild(productTable);
+
+	var j = 1;
+	for (i = 0; i < ele.length; i++) {
+		if (ele[i].checked) {
+			chosenProducts.push(ele[i].value);
+			var newRow = document.createElement("tr");
+			newRow.id += (j++ % 2 == 1) ? "oddRow" : "evenRow";
+			var productName = document.createElement("td");
+			productName.innerText = ele[i].value;
+			newRow.appendChild(productName);
+			productTable.appendChild(newRow);
+		}
+	}
+
+	c.style = "padding:5px;";
+	c.appendChild(document.createElement("br"))
+	// add paragraph and total price
+	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+
+}
